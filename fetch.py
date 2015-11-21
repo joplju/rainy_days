36bc6ddbbfa64c3ec52b77ce2e4ff8593c9d2566
Justin Joplin <joplin.justin@gmail.com>
2015-11-20 21:43:31 -0600

    Starting some layout

---
+++ fetch.py
@@ -0,0 +1,42 @@
+# http://www.ogimet.com/display_metars2.php?
+#     lang=en&
+#     lugar=KSGR&
+#     tipo=SA&
+#     ord=REV&
+#     nil=SI&
+#     fmt=txt&
+#     ano=2015&
+#     mes=11&
+#     day=20&
+#     hora=03&
+#     anof=2015&
+#     mesf=11&
+#     dayf=21&
+#     horaf=03&
+#     minf=59&
+#     send=send
+
+# import pymetar
+
+
+def parse_args():
+    pass
+
+
+def start_time():
+    pass
+
+
+def end_time():
+    pass
+
+
+def get_obs():
+    pass
+
+
+if __name__ == '__main__':
+    parse_args()
+    start_time()
+    end_time()
+    get_obs()
